762979417d0a4b517423ec7aa2957a895da22169
rayGreycloud <rayGreycloud@gmail.com>
2016-07-25 13:33:42 -0700

    Add users reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import usersReducer from './users';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  user: usersReducer
 });
 
 export default rootReducer;
