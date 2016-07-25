6d61fdd8105cc41e0c99cefab748b707bbd85682
rayGreycloud <rayGreycloud@gmail.com>
2016-07-25 13:25:29 -0700

    Add boilerplate react redux app

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Basic React Redux App for Middleware Demo</div>
+    );
+  }
+}
