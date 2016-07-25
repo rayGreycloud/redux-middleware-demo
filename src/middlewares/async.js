951b78b513d1d13fe96a92a0be2ac4cf2f6ea492
rayGreycloud <rayGreycloud@gmail.com>
2016-07-25 16:06:39 -0700

    Add async middleware to resolve API promise return

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,17 @@
 export default function ({ dispatch }) {
   return next => action => {
-    console.log(action);
-
-    next(action);
+    // if action has no payload or
+    // payload doesn't have .then send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    // Make sure action promise resolves
+    action.payload
+      .then(function (response) {
+        // create new action and replace promise
+        // with response data
+        const newAction =  {...action, payload: response };
+        dispatch(newAction);
+      });
   };
 }
