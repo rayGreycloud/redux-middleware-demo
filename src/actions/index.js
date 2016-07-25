1174136f973d3c346eeed7f6e4fd1329e7313b7a
rayGreycloud <rayGreycloud@gmail.com>
2016-07-25 14:58:17 -0700

    Add middleware skeleton

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,24 @@
+import axios from 'axios';
+
 import {
   FETCH_USERS
 } from './types';
 
-// static data for App
+// static data for App for initial scaffolding
+// export function fetchUsers() {
+//   return {
+//     type: FETCH_USERS,
+//     payload: [
+//       { name: 'Jane' },
+//       { name: 'Alex' },
+//       { name: 'Jim' }
+//     ]
+//   };
+// }
 export function fetchUsers() {
+  const request = axios.get('http://jsonplaceholder.typicode.com/users');
   return {
     type: FETCH_USERS,
-    payload: [
-      { name: 'Jane' },
-      { name: 'Alex' },
-      { name: 'Jim' }
-    ]
+    payload: request
   };
 }
